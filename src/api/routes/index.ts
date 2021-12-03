d4135f28a4b0a4f406edf639ba752afb0c60e38c
JiveOff <antoine@jiveoff.fr>
2021-12-03 07:06:34 +0100

    assasasa

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -4,10 +4,12 @@ const router = express.Router();
 import Personnes = require("./Personnes.routes");
 import Bateaux = require("./Bateaux.routes");
 import Sauvetages = require("./Sauvetages.routes");
+import Textes = require("./Textes.routes");
 
 router.use("/", Personnes);
 router.use("/", Bateaux);
 router.use("/", Sauvetages);
+router.use("/", Textes);
 
 router.get("/", (req, res) => {
   res.status(200).json({ status: "OK" });
