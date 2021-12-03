8d61d69ab04c5f913a3031dd06110f0111777c07
JiveOff <antoine@jiveoff.fr>
2021-12-03 05:41:32 +0100

    routeurs

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,8 +2,12 @@ import * as express from "express";
 const router = express.Router();
 
 import Personnes = require("./Personnes.routes");
+import Bateaux = require("./Bateaux.routes");
+import Sauvetages = require("./Sauvetages.routes");
 
 router.use("/", Personnes);
+router.use("/", Bateaux);
+router.use("/", Sauvetages);
 
 router.get("/", (req, res) => {
   res.status(200).json({ status: "OK" });
